3ffd0133afdc1e1e2d0e548287526131ec1b4645
georgewood749 <georgewood749@gmail.com>
2022-10-21 12:48:19 +0100

    set up basic structure, server running

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const server = express()
+
+let cors = require("cors")
+server.use(cors())
+
+const bodyParser = require('body-parser')
+server.use(bodyParser.json())
+
+// server.use(function (req, res, next) {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With, Origin');
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+//     next();
+// });
+
+
+server.get('/', (req, res) => {
+    res.send('Welcome to the DaBoiz Habit Tracker!')
+})
+
+
+module.exports = server;
