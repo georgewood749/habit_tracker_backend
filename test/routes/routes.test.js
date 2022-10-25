3ae81aa88918162c4bbb46edb5a7e4d871b86772
georgewood749 <georgewood749@gmail.com>
2022-10-25 14:37:13 +0100

    added test cases for endpoints

--- test/routes/routes.test.js
+++ test/routes/routes.test.js
@@ -17,54 +17,78 @@ describe('habits endpoints', () => {
     });
 
     test('should return a list of all users in database', async () => {
-        const res = await request(dbConfig).get('/users');
+        const res = await request(app).get('/users');
         expect(res.statusCode).toEqual(200);
         expect(res.body.length).toEqual(6);
     });
 
-    it('should post new user to the database', async () => {
+    it('should create a new user on the database', async () => {
         const res = await request(app).post('/');
-        expect(res.statusCode).toEqual(200);
-        expect(res.body.length).toEqual(6);
+        expect(res.statusCode).toEqual(201);
     });
+
+    it('should get the data associated with specific user', async () => {
+        const res = await request(app).get('/test');
+        expect(res.statusCode).toEqual(200);
+
+    });
+
+    it('should get data about all habits associated with selected user', async () => {
+        const res = await request(app).get('/test/habits');
+        expect(res.statusCode).toEqual(200);
+
+    });
+
+    it('should get data for specific habit for selected user', async () => {
+        const res = await request(app).get('/test/habits/running');
+        expect(res.statusCode).toEqual(200);
+
+    });
+
+    it('should update habit data in user data on database', async () => {
+        const res = await request(app).patch('/test/habits/running');
+        expect(res.statusCode).toEqual(200);
+
+    });
+
+    it('should delete habit from user in the database', async () => {
+        const res = await request(app).delete('/test/habits/running');
+        expect(res.statusCode).toEqual(204);
+
+    });
+
+        it('responds to invalid username with 404', (done) => {
+        request(api).get('/users/invalidUsername').expect(404, done)
+    })
 })
 
-// describe('API server', () => {
-//     let api;
+// const index = require("../index");
 
-//     beforeAll( async () => {
-//         api = app.listen(5000, () => {
-//             console.log(`Example app listening on port 5000`)
-//         })
-//     })
+// const request = require("supertest");
+// const express = require("express");
+// const app = express();
 
-//     afterAll(async (done) => {
-//         console.log('Stopping test server');
-//         api.close(done)
-//     })
+// app.use(express.urlencoded({ extended: false }));
+// app.use("/", index);
 
-//     it('responds to get / with status of 200', (done) => {
-//         request(api).get('/').expect(200, done);
-//     })
+// test("index route works", done => {
+//   request(app)
+//     .get("/")
+//     .expect("Content-Type", /json/)
+//     .expect({ name: "frodo" })
+//     .expect(200, done);
+// });
 
-//     it('retrieves all cats', (done) => {
-//         request(api).get('/cats').expect(200, done);
-//     })
+// test("testing route works", done => {
+//   request(app)
+//     .post("/test")
+//     .type("form")
+//     .send({ item: "hey" })
+//     .then(() => {
+//       request(app)
+//         .get("/test")
+//         .expect({ array: ["hey"] }, done);
+//     });
+// });
 
-//     it('retrieves a specific cat', (done) => {
-//         request(api).get('/cats/2').expect(200)
-//             .expect({
-//                 id: 2,
-//                 name: 'Zelda',
-//                 image: '../assets/cat2.png'
-//             }, done)
-//     })
-
-//     it('responds to invalid methods with 405', (done) => {
-//         request(api).post('/cats').expect(405, done)
-//     })
-
-//     it('responds to invalid id with 404', (done) => {
-//         request(api).post('/cats/5').expect(404, done)
-//     })
-// })
+// https://www.theodinproject.com/lessons/nodejs-testing-routes-and-controllers
