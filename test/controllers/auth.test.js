0df73bad96eb1454b2f16f12a37c8ac4005f4927
georgewood749 <georgewood749@gmail.com>
2022-10-26 18:32:02 +0100

    updated test suites

--- test/controllers/auth.test.js
+++ test/controllers/auth.test.js
@@ -1,2 +1,63 @@
 const Habit = require('../../model/model')
+const auth = require('../../controllers/auth')
+const crypto = require('crypto')
+const bcrypt = require('bcryptjs')
+const {register, login} = require('../../controllers/auth')
 
+describe('authentication tests', () => {
+    it('should generate identical hash given same password text and salt', () => {
+        try {
+            let salt = Habit.generateSalt()
+            let hash = Habit.generateHash('password', salt)
+            expect(hash).toEqual(Habit.generateHash('password', salt))
+        } catch (err) {
+            throw new Error(err)
+        }
+    })
+
+    it('should throw an error if user leaves password field empty', async () => {
+        try {
+            await new Habit({
+                username: "test",
+                password: ""
+            }).save()
+        } catch (err) {
+            expect(err)
+        }
+    })
+
+    it('should throw authentication error if user enters incorrect password', async () => {
+        try {
+            // await new Habit({ username: "test", password: 'password' }).save()
+            let salt = Habit.generateSalt()
+            let hashedPassword = Habit.generateHash('password', salt)
+            const auth = Habit.authenticate("passw0rd", hashedPassword, salt)
+            expect(auth).toEqual(false)
+        } catch (err) {
+            throw new Error(err)
+        }
+    })
+
+    it('should authenticate if user enters correct password', async () => {
+        try {
+            // await new Habit({ username: "test", password: 'password' }).save()
+            let salt = Habit.generateSalt()
+            let hashedPassword = Habit.generateHash('password', salt)
+            const auth = Habit.authenticate("password", hashedPassword, salt)
+            expect(auth).toEqual(true)
+        } catch (err) {
+            throw new Error(err)
+        }
+    })
+})
+
+// describe('auth controller tests', () => {
+//     it('should register a new user', async () => {
+//         newUser = register({username: 'test', password: 'password'}, 201)
+//         expect(201)
+//     })
+
+//     it('should log a user in if correct credentials are entered', async () => {
+
+//     })
+// })
--- test/controllers/habits.test.js
+++ test/controllers/habits.test.js
@@ -9,121 +9,121 @@ const userData = {
     username: "testuser",
     password: "912345678",
     habits: [
-      { id: 1, habit: "Eat", streak: 31, isCompleted: true },
-      {
-        id: 2,
-        habit: "Sleep",
-        frequency: "Daily",
-        streak: 31,
-        isCompleted: false,
-      },
-      {
-        id: 3,
-        habit: "Gym",
-        frequency: "Every 2 days",
-        streak: 25,
-        isCompleted: true,
-      },
+        { id: 1, habit: "Eat", streak: 31, isCompleted: true },
+        {
+            id: 2,
+            habit: "Sleep",
+            frequency: "Daily",
+            streak: 31,
+            isCompleted: false,
+        },
+        {
+            id: 3,
+            habit: "Gym",
+            frequency: "Every 2 days",
+            streak: 25,
+            isCompleted: true,
+        },
     ],
-  };
+};
 
 const validUser = {
-  username: "George",
-  password: "password",
+    username: "George",
+    password: "password",
 };
 
 const invalidUser = {
-  username: "George",
-  password: "",
+    username: "George",
+    password: "",
 };
 
-  beforeAll(async () => {
+beforeAll(async () => {
     await connect();
-  });
+});
 
-  beforeEach(async () => {
+beforeEach(async () => {
     await clear();
-  });
+});
 
-  afterAll(async () => {
+afterAll(async () => {
     await close();
-  });
+});
 
-  describe("getAll", () => {
+describe("getAll", () => {
     test("it returns all users and their habits with a 200 status code", async () => {
-       await supertest(server).get(`/`).expect(200);
-      });
+        await supertest(server).get(`/`).expect(200);
     });
+});
 
-  describe("show", () => {
+describe("show", () => {
     test("it returns all details about a user", async () => {
         await supertest(server).get(`/testuser`);
         expect(200);
         expect(userData);
     })
-  })
+})
 
-  describe("getHabits", () => {
+describe("getHabits", () => {
     test("it returns the habits of a user", async () => {
         await supertest(server).get(`/testuser/habits`);
         expect(200);
         expect(userData.habits);
     })
-  })
+})
 
-  describe("getHabit", () => {
+describe("getHabit", () => {
     test("it returns all details about a user", async () => {
         await supertest(server).get(`/testuser/habits/3`);
         expect(200);
         expect(userData.habits[2]);
     })
-  })
+})
 
-//   describe("editHabit", () => {
+// describe("editHabit", () => {
 //     test("it should create a new habit", async () => {
 //         let newHabit = {
-//             id: 4, habit: "Cold shower", frequency: "Every 2nd day",isCompleted: false
+//             id: 4, habit: "Cold shower", frequency: "Every 2nd day", isCompleted: false
 //         }
 //         await supertest(server).patch(`/testuser/habits/4`).send(newHabit);
 //         expect(201);
 //         expect(userData.habits).toHaveLength(4)
 //     })
-//   })
-describe("deleteHabit", () => {
-    test("it deletes a specific habit based on id", async () => {
-        await supertest(server).delete(`/testuser/habits/2`);
-        expect(204);
-        expect(userData.habits).toHaveLength(2);
-    })
-  })
+// })
+// describe("deleteHabit", () => {
+//     test("it deletes a specific habit based on id", async () => {
+//         await supertest(server).delete(`/testuser/habits/2`);
+//         expect(204);
+//         expect(userData.habits).toHaveLength(2);
+//     })
+// })
 
 
-  describe("valid create", () => {
+describe("valid create", () => {
     test("user should be created if validation passes", async () => {
-      let error = null;
+        let error = null;
 
-      try {
-        const user = new Habit(validUser);
-        await user.validate();
-      } catch (e) {
-        error = e;
-      }
+        try {
+            const user = new Habit(validUser);
+            await user.validate();
+        } catch (e) {
+            error = e;
+        }
 
-      expect(error).toBeNull();
+        expect(error).toBeNull();
     });
-  });
+});
 
-  describe("invalid create", () => {
+describe("invalid create", () => {
     test("user registration should fail if password string is empty", async () => {
-      let error = null;
+        let error = null;
 
-      try {
-        const user = new Habit(invalidUser);
-        await user.validate();
-      } catch (e) {
-        error = e;
-      }
+        try {
+            const user = new Habit(invalidUser);
+            await user.validate();
+        } catch (e) {
+            error = e;
+        }
 
-      expect(error).not.toBeNull();
+        expect(error).not.toBeNull();
     });
-  });
+});
